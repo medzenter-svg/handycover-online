8fcbd4eb2cf936d75eba26d110aa733f34529d1d
medzenter-svg <medzenter@icloud.com>
2026-07-17 19:53:49 +0200

    Add files via upload

---
+++ online-enhance.js
@@ -0,0 +1,7 @@
+(()=>{
+  const header=document.querySelector('header');
+  if(header){
+    const brandLeaf=[...header.querySelectorAll('*')].find(el=>el.children.length===0&&/handycover\.com/i.test(el.textContent||''));
+    if(brandLeaf)brandLeaf.textContent=brandLeaf.textContent.replace(/handycover\.com/i,'HandyCover.Online');
+  }
+})();
